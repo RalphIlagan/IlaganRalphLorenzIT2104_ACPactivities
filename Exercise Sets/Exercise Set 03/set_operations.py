5d184d8c283f7e9a66e0be54b4f58373b5942149
RalphIlagan <23-06919@g.batstate-u.edu.ph>
2024-09-11 12:25:20 +0800

    Add files via upload

---
+++ Exercise Sets/Exercise Set 03/set_operations.py
@@ -0,0 +1,22 @@
+set1 = {8, 16, 24, 32, 44}
+set2 = {7, 14, 8, 32, 21}
+set_difference = set1 - set2
+set_difference1= set2 - set1
+union_set = set1 | set2
+symmetric_difference = set1 ^ set2
+symmetric_difference1 = set2 ^ set1
+set_interesection = set1 & set2
+set_interesection1 = set2 & set1
+
+print ("SET DIFFERENCE")
+print (set_difference)
+print (set_difference1)
+print ("UNION SETS")
+print (union_set)
+print ("SYMMETRIC DIFFERENCE")
+print (symmetric_difference)
+print (symmetric_difference1)
+print("SET INTERSECTION")
+print (set_interesection)
+print (set_interesection1)
+
