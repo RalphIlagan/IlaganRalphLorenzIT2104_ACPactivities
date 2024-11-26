c059fcb485d1bb48cc0d229da92025a6bce4ee71
RalphIlagan <23-06919@g.batstate-u.edu.ph>
2024-11-26 19:51:24 +0800

    Add files via upload

---
+++ Laboratory Activity/Laboratory Activity 4– {26}-{11}-{2024}/Capybara/script.py
@@ -0,0 +1,15 @@
+from Capybara import Capybara
+
+capybara1 = Capybara("Biscoff", "M", 5)
+capybara2 = Capybara("Churro", "F", 3)
+capybara3 = Capybara("Cinnamon", "M", 7)
+
+capybaras = [capybara1, capybara2, capybara3]
+
+test_case = int(input("Enter the test case number: "))
+
+if 1 <= test_case <= len(capybaras):
+    capybara = capybaras[test_case - 1]
+    print(f"Test case {test_case}: Name: {capybara.name}, Gender: {capybara.gender}, Age: {capybara.age} years old")
+else:
+    print("Invalid test case number.")
